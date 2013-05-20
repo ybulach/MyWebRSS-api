d7887c85cf16ac984d3a9d35728db43bd80d0439
Yamine BULACH <oozinotshak@gmail.com>
2013-05-20 16:20:31 +0200

    Cleaning Tokens now works for Tokens older than 30 days
    Corrected AddFeed page
    Generic API .htaccess modified

--- cron/cleanTokens.php
+++ cron/cleanTokens.php
@@ -5,7 +5,7 @@ require_once("lib.php");
 
 try {
 	// Delete the expired Tokens (over 30 days old)
-	$date = time();
+	$date = time() - 60*60*24*30;
 	$delete = $mysql->prepare("DELETE FROM tokens WHERE token_date < :date");
 	$delete->bindParam(":date", $date);
 	
