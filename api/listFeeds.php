9c17f5ab10aeced48d7bb4e323b9db078fc41809
Yamine BULACH <oozinotshak@gmail.com>
2013-06-07 12:54:11 +0200

    Auto-refresh feeds list and show unread articles count

--- api/listFeeds.php
+++ api/listFeeds.php
@@ -13,6 +13,24 @@ try {
 		throw new Exception("Could not list feed. Try again later");
 	
 	$json_result["result"] = $select->fetchAll(PDO::FETCH_ASSOC);
+	
+	// Add the unread articles count
+	for($i = 0; $i < $count($json_result["result"]); $i++) {
+		$json_result["result"][$i]["unread"] = 0;
+		
+		$select = $mysql->prepare("SELECT COUNT(feed_ref) as unread FROM user_articles INNER JOIN articles ON article_id=article_ref WHERE user_ref=:user AND feed_ref=:feed");
+		$select->bindParam(":user", $user);
+		$select->bindParam(":feed", $json_result["result"][$i]["id"]);
+		
+		if($select->execute()) {
+			$unread = $select->fetch();
+			
+			if($unread) {
+				$unread = $unread["unread"];
+				$json_result["result"][$i]["unread"] = $unread;
+			}
+		}
+	}
 }
 catch( Exception $e ){
 	send_error($e->getMessage());
