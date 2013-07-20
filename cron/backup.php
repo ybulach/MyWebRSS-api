aa74cbb0218f4bb869aebfc3f041d5de3490bd68
Yamine BULACH <oozinotshak@gmail.com>
2013-07-21 01:04:03 +0200

    Delete old database backups in cron file

--- cron/backup.php
+++ cron/backup.php
@@ -7,6 +7,9 @@ try {
 	// Delete the unused feeds
 	$date = date("Y-m-d_H-i-s");
 	exec($mysqldump." --user=".$mysql_user." --password=".$mysql_pass." --host=".$mysql_host." ".$mysql_base." > ".$backups_dir."/".$date);
+	
+	// Delete old backups
+	exec("find ".$backups_dir." -type f -mtime +7 | xargs rm");
 }
 catch( Exception $e ){
 	send_error($e->getMessage());
