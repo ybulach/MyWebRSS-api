f9d5582b1765cfbae598768145cc29e6c1031b45
Yamine BULACH <oozinotshak@gmail.com>
2013-07-21 14:10:38 +0200

    Improved unread articles count and display of feeds error

--- api/feedList.php
+++ api/feedList.php
@@ -6,7 +6,7 @@ try {
 	$user = check_token($_GET["token"]);
 	
 	// Get the feeds
-	$select = $mysql->prepare("SELECT feed_id as id, feed_title as title, feed_description as description FROM user_feeds INNER JOIN feeds ON feed_id=feed_ref WHERE user_ref=:user ORDER BY feed_title ASC");
+	$select = $mysql->prepare("SELECT feed_id as id, feed_title as title, feed_description as description, feed_error as error FROM user_feeds INNER JOIN feeds ON feed_id=feed_ref WHERE user_ref=:user ORDER BY feed_title ASC");
 	$select->bindParam(":user", $user);
 	
 	if(!$select->execute())
